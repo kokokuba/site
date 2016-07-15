72a6fac401931c3609fa2c692eae3669af5b6946
Lucas <lucas@agtlucas.com>
2016-07-14 21:13:33 -0300

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp')
+const cssnano = require('cssnano')
+const mqpacker = require('css-mqpacker')
+const scss = require('postcss-scss')
+const nested = require('postcss-nested')
+const cssnext = require('postcss-cssnext')
+const atImport = require('postcss-import')
+const focus = require('postcss-focus')
+const fontMagician = require('postcss-font-magician')
+const postcss = require('gulp-postcss')
+
+gulp.task('css', () => {
+  var processors = [
+    atImport,
+    cssnext({ browsers: ['last 2 versions'] }),
+    fontMagician,
+    focus,
+    nested,
+    mqpacker,
+    cssnano({ autoprefixer: false })
+  ]
+
+  return gulp.src('src/css/main.css')
+    .pipe(postcss(processors, { syntax: scss }))
+    .pipe(gulp.dest('_includes/css'))
+})
+
+gulp.task('watch', () => gulp.watch('src/css/**/*.css', ['css']))
+
+gulp.task('default', ['css', 'watch'])
